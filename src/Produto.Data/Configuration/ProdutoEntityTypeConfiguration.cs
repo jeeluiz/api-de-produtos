444d8c229a9929f18546cdd8d3683089c7380732
Jeferson Luiz <jl_jau@hotmail.com>
2025-03-23 19:33:14 -0300

    feat(seed): adiciona novos produtos ao seed inicial
    - Adiciona 5 produtos ao seed inicial do banco de dados
    - Atualiza as migrations para refletir as mudanças no seed

--- src/Produto.Data/Configuration/ProdutoEntityTypeConfiguration.cs
+++ src/Produto.Data/Configuration/ProdutoEntityTypeConfiguration.cs
@@ -29,5 +29,43 @@ public class ProdutoEntityTypeConfiguration : IEntityTypeConfiguration<ProdutoEn
 
         builder.Property(p => p.Estoque)
            .IsRequired();
+
+        builder.HasData([
+            new ProdutoEntity(
+                nome: "Produto 1",
+                descricao: "Descrição do Produto 1",
+                valor: 10.00m,
+                estoque: 100){
+                Id = new Guid("0195c525-9a61-711c-8f23-fe5648bb690a")
+            },
+            new ProdutoEntity(
+                nome: "Produto 2",
+                descricao: "Descrição do Produto 2",
+                valor: 20.00m,
+                estoque: 200){
+                Id = new Guid("0195c525-9a61-711c-8f24-03f9b0e3cd25")
+            },
+            new ProdutoEntity(
+                nome: "Produto 3",
+                descricao: "Descrição do Produto 3",
+                valor: 30.00m,
+                estoque: 300){
+                Id = new Guid("0195c525-9a61-711c-8f24-0592096c4b61")
+            },
+            new ProdutoEntity(
+                nome: "Produto 4",
+                descricao: "Descrição do Produto 4",
+                valor: 40.00m,
+                estoque: 400){
+                Id = new Guid("0195c525-9a61-711c-8f24-0b3d685cc7d6")
+            },
+            new ProdutoEntity(
+                nome: "Produto 5",
+                descricao: "Descrição do Produto 5",
+                valor: 50.00m,
+                estoque: 500){
+                Id = new Guid("0195c525-9a61-711c-8f24-0c11233469cf")
+            }
+        ]);
     }
 }
---
+++ src/Produto.Data/Migrations/20250323223155_newseed.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Produto.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class newseed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "produtos",
+                columns: new[] { "Id", "Descricao", "Estoque", "Nome", "Valor" },
+                values: new object[,]
+                {
+                    { new Guid("0195c525-9a61-711c-8f23-fe5648bb690a"), "Descrição do Produto 1", 100, "Produto 1", 10.00m },
+                    { new Guid("0195c525-9a61-711c-8f24-03f9b0e3cd25"), "Descrição do Produto 2", 200, "Produto 2", 20.00m },
+                    { new Guid("0195c525-9a61-711c-8f24-0592096c4b61"), "Descrição do Produto 3", 300, "Produto 3", 30.00m },
+                    { new Guid("0195c525-9a61-711c-8f24-0b3d685cc7d6"), "Descrição do Produto 4", 400, "Produto 4", 40.00m },
+                    { new Guid("0195c525-9a61-711c-8f24-0c11233469cf"), "Descrição do Produto 5", 500, "Produto 5", 50.00m }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "produtos",
+                keyColumn: "Id",
+                keyValue: new Guid("0195c525-9a61-711c-8f23-fe5648bb690a"));
+
+            migrationBuilder.DeleteData(
+                table: "produtos",
+                keyColumn: "Id",
+                keyValue: new Guid("0195c525-9a61-711c-8f24-03f9b0e3cd25"));
+
+            migrationBuilder.DeleteData(
+                table: "produtos",
+                keyColumn: "Id",
+                keyValue: new Guid("0195c525-9a61-711c-8f24-0592096c4b61"));
+
+            migrationBuilder.DeleteData(
+                table: "produtos",
+                keyColumn: "Id",
+                keyValue: new Guid("0195c525-9a61-711c-8f24-0b3d685cc7d6"));
+
+            migrationBuilder.DeleteData(
+                table: "produtos",
+                keyColumn: "Id",
+                keyValue: new Guid("0195c525-9a61-711c-8f24-0c11233469cf"));
+        }
+    }
+}
